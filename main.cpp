8b38498a6baf920c5ba7dcc1938fad01c5e905a7
Mani-Amanda <135225894+Mani-Amanda@users.noreply.github.com>
2024-01-10 10:53:56 +0530

    Create class Cell

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@
 
 using namespace std;
 
+class Cell {
+public:
+    bool isMine;
+    bool isRevealed;
+    bool isFlagged;
+    int adjacentMines;
+
+    Cell() : isMine(false), isRevealed(false), isFlagged(false), adjacentMines(0) {}
+};
+
 int main(){
 
     return 0;
