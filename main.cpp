9ae5ef02501c115c8af8bb5882f373807209a703
sathmipeiris <sathmipeiris@gmail.com>
2024-01-10 10:56:59 +0530

    Mine sweepwe board class attributeas and + place mines

--- main.cpp
+++ main.cpp
@@ -15,6 +15,25 @@ public:
     Cell() : isMine(false), isRevealed(false), isFlagged(false), adjacentMines(0) {}
 };
 
+class MinesweeperBoard {
+private:
+    int rows;
+    int cols;
+    vector<vector<Cell>> board;
+
+    void placeMines(int numMines) {
+        int totalCells = rows * cols;
+        while (numMines > 0) {
+            int randRow = rand() % rows;
+            int randCol = rand() % cols;
+            if (!board[randRow][randCol].isMine) {
+                board[randRow][randCol].isMine = true;
+                numMines--;
+            }
+        }
+    }
+};
+
 int main(){
 
     return 0;
